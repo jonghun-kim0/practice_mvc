4776c98c91f7c5b04d00287e7559cf4083ff72c8
jonghun_kim <kjonghun0828@g.hongik.ac.kr>
2025-07-04 14:35:30 +0900

    chore : first init

---
+++ src/test/java/com/metaverse/practice_mvc/PracticeMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metaverse.practice_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
