a6f4627c448f695dcbaa0dcaa7207086c76c6a78
jonghun_kim <kjonghun0828@g.hongik.ac.kr>
2025-07-07 15:23:29 +0900

    feat : JSON RestController 적용

--- src/main/java/com/metaverse/practice_mvc/controller/DataResponeController.java
+++ src/main/java/com/metaverse/practice_mvc/controller/DataResponeController.java
@@ -1,19 +1,16 @@
 package com.metaverse.practice_mvc.controller;
 import com.metaverse.practice_mvc.domain.Star;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class DataResponeController {
     @GetMapping("response/json/string")
-    @ResponseBody
     public String helloStringJson(){
         return "{\"name\" : \"Meta\", \"age\" = 95 }";
     }
 
     @GetMapping("response/json/instance")
-    @ResponseBody
     public Star helloInstanceJson(){
         //Star star = new Star();
         return new Star("Meta",88);
