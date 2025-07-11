42439520488faa3978a2b85e1057ae0239f646ea
jonghun_kim <kjonghun0828@g.hongik.ac.kr>
2025-07-11 17:32:30 +0900

    feat : generic 페이지 추가, main 페이지 내용 개선

--- src/main/java/com/metaverse/practice_mvc/controller/HomeController.java
+++ src/main/java/com/metaverse/practice_mvc/controller/HomeController.java
@@ -9,4 +9,9 @@ public class HomeController {
     public String home() {
         return "index";
     }
+
+    @GetMapping("/generic")
+    public String generic() {
+        return "generic";
+    }
 }
